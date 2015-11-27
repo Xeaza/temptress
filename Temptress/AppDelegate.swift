777d3ef63e1bc09d6c74413e5757b23ec5e6dc5d
xeaza <taylorwrightsanson@gmail.com>
2015-11-27 11:28:27 -0500

    Adding crashlitics

--- Temptress/AppDelegate.swift
+++ Temptress/AppDelegate.swift
@@ -7,16 +7,16 @@
 //
 
 import UIKit
+import Fabric
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-//        UIApplication.sharedApplication().setStatusBarStyle(.LightContent, animated: false)
-        
+        Fabric.with([Crashlytics.self])
         return true
     }
 
